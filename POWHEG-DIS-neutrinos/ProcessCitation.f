21455dabf57deae21289c92cb675fc6166d9cd74
vschutze <151664555+vschutze@users.noreply.github.com>
2024-05-30 12:48:49 +0200

    Delete POWHEG-DIS-neutrinos directory
    This POWHEG version still didn't have neutrinos implemented

--- POWHEG-DIS-neutrinos/ProcessCitation.f
+++
@@ -1,9 +0,0 @@
-c  -*- Fortran -*-
-      write(*,*) "************************            AND             ************************"
-      write(*,*) "****                                                                    ****"
-      write(*,*) "**** The implementation of this process is discussed in                 ****"
-      write(*,*) "****                                                                    ****"
-      write(*,*) "**** arXiv:2309.02127, Andrea Banfi, Silvia Ferrario Ravasio, Barbara   ****"
-      write(*,*) "**** Jäger, Alexander Karlberg, Felix Reichenbach, Giulia Zanderighi    ****"
-      write(*,*) "****                                                                    ****"
-      write(*,*) "****************************************************************************"
--- POWHEG-DIS-neutrinos/include/pwhg_pdfinfo.h
+++
@@ -1,6 +0,0 @@
-c -*- Fortran -*-
-      integer pdfinfo_f1,pdfinfo_f2
-      real * 8 pdfinfo_x1,pdfinfo_x2,pdfinfo_xf1,pdfinfo_xf2,pdfinfo_scale
-      common/pwhg_pdfinfo/pdfinfo_f1,pdfinfo_f2,pdfinfo_x1,pdfinfo_x2,pdfinfo_xf1,
-     1 pdfinfo_xf2,pdfinfo_scale
-      save /pwhg_pdfinfo/
