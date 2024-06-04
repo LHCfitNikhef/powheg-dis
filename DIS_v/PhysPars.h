c265e5af992330295ac65a0bcb2ea46094d00b8f
vschutze <151664555+vschutze@users.noreply.github.com>
2024-06-04 15:54:52 +0200

    Update PhysPars.h
    add ph_gfermi to be able to call it in other files

--- DIS_v/PhysPars.h
+++ DIS_v/PhysPars.h
@@ -1,5 +1,5 @@
 c -*- Fortran -*-
-      real * 8 ph_alphaem,ph_Zmass,ph_Zwidth,ph_Wmass,ph_Wwidth,ph_cthw,
+      real * 8 ph_alphaem,ph_Zmass,ph_Zwidth,ph_Wmass,ph_Wwidth,ph_cthw, ph_gfermi,
      $     ph_sthw,ph_sthw2,ph_Zmass2,ph_Zmass2low,ph_Zmass2high,
      $     ph_Wmass2,ph_Wmass2low,ph_Wmass2high ,ph_ZmZw,ph_WmWw,
      $     ph_unit_e, ph_CKM(3,3), ph_CKM_matrix(6,6)
